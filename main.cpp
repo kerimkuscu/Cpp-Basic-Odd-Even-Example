13c81e8a644ed9b9b2e871c4a2ea4a10fd2b59e0
Kerim Kuşcu <43302726+kerimkuscu@users.noreply.github.com>
2019-04-14 17:13:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,10 +13,10 @@ int main(int argc, char** argv) {
     
     if (number % 2 == 0) {
     cout << "Your number = " << number << endl;
-   	cout << "The number you entered is even.";
+    cout << "The number you entered is even.";
     } else {
     cout << "Your number = " << number << endl;
-   	cout << "The number you entered is odd.";
+    cout << "The number you entered is odd.";
     }
     
 	return 0;
