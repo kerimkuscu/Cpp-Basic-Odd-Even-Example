2a9fb075afb4cc9e06e32a18e1fed8ccbc54991e
Kerim <kerimkuscu95@gmail.com>
2019-04-14 17:12:55 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    int number;
+    
+    cout << "Please entered number = ";
+    cin >> number;
+    
+    if (number % 2 == 0) {
+    cout << "Your number = " << number << endl;
+   	cout << "The number you entered is even.";
+    } else {
+    cout << "Your number = " << number << endl;
+   	cout << "The number you entered is odd.";
+    }
+    
+	return 0;
+}
